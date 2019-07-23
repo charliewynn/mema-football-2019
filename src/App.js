15c4c6be646e105538c8a7ecd2779e833812e98a
Charlie Wynn <charlie.wynn@technivation.com>
2019-07-23 11:12:27 -0500

    updated readme

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
   const playersApi = 'https://api.sheety.co/d50d6864-69d7-46b3-b39d-a4ea29480254';
   const scoresApi = 'https://api.sheety.co/63dbeee0-3aa4-4e79-ac39-545ac3ee7b86';
   useEffect(()=>{
-
     fetch(scoresApi).then(d=>d.json()).then(d=>{
       setScores(d);
     });
