a5982fc43e198ccc71544f5d78c9fb4d31b75185
Jordan Miller <jordan.miller@hrblock.com>
2019-08-28 12:56:37 -0500

    added player card functionallity

--- src/Components/PlayerCard.js
+++ src/Components/PlayerCard.js
@@ -4,22 +4,12 @@ function PlayerCard({ player }) {
   return (
     <div className="game">
       <div className="title">
-        OU
-        {player.home ? " vs " : " @ "}
-        {player.team}
+        {player.name}
       </div>
-      <div>{player.date}</div>
-      <div>{JSON.stringify(player.ouWon)}</div>
-      {player.complete && (
-        <>
-          <div>
-            {player.ouScore} to {player.oppScore}
-          </div>
-          <div>Guessed Winner: {player.guessedWinner.join(" ")}</div>
-          <div>Closest to OU score: {player.closestOu.join(" ")}</div>
-          <div>Closest to Opp score: {player.closestOpp.join(" ")}</div>
-        </>
-      )}
+      <div className="title">
+        Player Score: 
+        {player.totalScore}
+      </div>
     </div>
   );
 }
