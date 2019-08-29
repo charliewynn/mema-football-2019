dae6e53c8a344c2f56361afe3f685da46b0c9dc9
Charlie Wynn <charlie.wynn@technivation.com>
2019-08-29 13:48:42 -0500

    refactored predictionsTable to es6

--- src/Components/PredictionsTable.js
+++ src/Components/PredictionsTable.js
@@ -1,81 +1,36 @@
 import React from "react";
 
-function PredictionsTable({ players, games }) {
-  var headers = GetTableHeaders(games);
-  var rows = GetPlayerPredictions(players);
+export default ({ players, games }) => (
+  <table align="center">
+    {GetTableHeaders(games)}
+    {GetPlayerPredictions(players)}
+  </table>
+);
 
-  return (
-    <table align="center">
-      {headers}
-      {rows}
-    </table>
-  );
-}
-
-function GetTableHeaders(games)
-{
-  var headers = 
+const GetTableHeaders = games => (
   <tr>
     <th>Player</th>
-    <th>Game1</th>
-    <th>Game2</th>
-    <th>Game3</th>
-    <th>Game4</th>
-    <th>Game5</th>
-    <th>Game6</th>
-    <th>Game7</th>
-    <th>Game8</th>
-    <th>Game9</th>
-    <th>Game10</th>
-    <th>Game11</th>
-    <th>Game12</th>
+    {games.map(g => (
+      <th>{g.team}</th>
+    ))}
   </tr>
-  if (games.length !== 0)
-  {
-    headers = 
+);
+
+const GetPlayerPredictions = players =>
+  players.map(p => (
     <tr>
-      <th>Player</th>
-      <th>{games[0].team}</th>
-      <th>{games[1].team}</th>
-      <th>{games[2].team}</th>
-      <th>{games[3].team}</th>
-      <th>{games[4].team}</th>
-      <th>{games[5].team}</th>
-      <th>{games[6].team}</th>
-      <th>{games[7].team}</th>
-      <th>{games[8].team}</th>
-      <th>{games[9].team}</th>
-      <th>{games[10].team}</th>
-      <th>{games[11].team}</th>
+      <td>{p.name}</td>
+      <td>{p.houston}</td>
+      <td>{p.southDakota}</td>
+      <td>{p.ucla}</td>
+      <td>{p.texasTech}</td>
+      <td>{p.kansas}</td>
+      <td>{p.texas}</td>
+      <td>{p.westVirginia}</td>
+      <td>{p.kansasState}</td>
+      <td>{p.iowaState}</td>
+      <td>{p.baylor}</td>
+      <td>{p.tcu}</td>
+      <td>{p.osu}</td>
     </tr>
-  }
-  return headers;
-}
-
-function GetPlayerPredictions(players)
-{
-  var rows = [];
-  players.forEach(function(p)
-  {
-    rows.push(
-      <tr> 
-        <td>{p.name}</td>
-        <td>{p.houston}</td>
-        <td>{p.southDakota}</td>
-        <td>{p.ucla}</td>
-        <td>{p.texasTech}</td>
-        <td>{p.kansas}</td>
-        <td>{p.texas}</td>
-        <td>{p.westVirginia}</td>
-        <td>{p.kansasState}</td>
-        <td>{p.iowaState}</td>
-        <td>{p.baylor}</td>
-        <td>{p.tcu}</td>
-        <td>{p.osu}</td>
-      </tr>
-    )
-  });
-  return rows;
-}
-
-export default PredictionsTable;
+  ));
