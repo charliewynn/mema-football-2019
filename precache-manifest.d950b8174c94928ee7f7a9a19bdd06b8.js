8a37d089d0c44bf48f72400818cffe5f83e3ac2e
Charlie Wynn <charlie.wynn@technivation.com>
2019-09-03 16:36:04 -0500

    Updates

---
+++ precache-manifest.d950b8174c94928ee7f7a9a19bdd06b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c2b4701480fb336b484da8b18b09eb8",
+    "url": "/mema-football-2019/index.html"
+  },
+  {
+    "revision": "bec24ca6f63ce12d7e79",
+    "url": "/mema-football-2019/static/css/main.715b8c70.chunk.css"
+  },
+  {
+    "revision": "6a37dba3d866e031d9b3",
+    "url": "/mema-football-2019/static/js/2.c0e38dd5.chunk.js"
+  },
+  {
+    "revision": "bec24ca6f63ce12d7e79",
+    "url": "/mema-football-2019/static/js/main.614fa219.chunk.js"
+  },
+  {
+    "revision": "17647e842e33fb447732",
+    "url": "/mema-football-2019/static/js/runtime~main.3d40c256.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mema-football-2019/precache-manifest.a899974fd738315e14264660eb17ea58.js"
+  "/mema-football-2019/precache-manifest.d950b8174c94928ee7f7a9a19bdd06b8.js"
 );
 
 self.addEventListener('message', (event) => {
